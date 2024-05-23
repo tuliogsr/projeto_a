a5c25f92c0e6ecf3a4ff51c038d27ab990d73ecc
Pedro Borges <pedroholiveiraborges@gmail.com>
2024-05-23 19:31:21 -0300

    implementação das classes na 'classes_ativos.hpp'

--- include/classes_ativos.hpp
+++ include/classes_ativos.hpp
@@ -4,14 +4,42 @@
 #include <iostream>
 #include <string>
 
+class Ativo {
+protected:
+    std::string nome;
+    double valor;
+
+public:
+    Ativo(const std::string& nome, double valor);
+    virtual ~Ativo();
+    
+    virtual void info() const;
+};
+
+class Acao : public Ativo {
+private:
+    int quantidade;
+
+public:
+    Acao(const std::string& nome, double valor, int quantidade);
+    virtual ~Acao();
+    
+    virtual void info() const override;
+};
+
+class Moeda : public Ativo {
+private:
+    std::string simbolo;
+
+public:
+    Moeda(const std::string& nome, double valor, const std::string& simbolo);
+    virtual ~Moeda();
+    
+    virtual void info() const override;
+};
 
 void classes_ativos();
 
 #endif
 
 //Era historia 3
-
-/*usar ponteiros interligantes
-separar em classes pai e filhas: ativos(pai), ações, moedas(filhas)
-
-*/ 
--- src/classes_ativos.cpp
+++ src/classes_ativos.cpp
@@ -1,9 +1,5 @@
 #include "classes_ativos.hpp"
 
-/*usar ponteiros interligantes
-separar em classes pai e filhas: ativos(pai), ações, moedas(filhas)
-*/
-
 void classes_ativos(){
   std::cout << "-----------//-----------" << std::endl;
   std::cout << "Acessar uma variedade de classes de ativos" << std::endl;
